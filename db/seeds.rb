3c881aa0a745e091280cc66a440241e21a554c7e
quang.le <quang.le@senioradvisor.com>
2018-01-27 22:28:00 -0600

    [r] update `rake db:seed`

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quang = User.create(name: 'quang le', website_url: 'https://www.aplaceformom.com/assisted-living/texas/austin')
+john = User.create(name: 'john doe', website_url: 'https://www.aplaceformom.com/assisted-living/texas/houston')
+jack = User.create(name: 'jack frost', website_url: 'https://www.aplaceformom.com/assisted-living/texas/dallas')
+jane = User.create(name: 'jane doe', website_url: 'https://www.aplaceformom.com/assisted-living/texas/waco')
+
+quang.friends << john
+jack.friends << quang
+
