e029e3572fe8991f8a61cc2711ecba3d3598e207
quang.le <quang.le@senioradvisor.com>
2018-01-27 23:36:57 -0600

    [f] Add form to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,31 @@ class UsersController < ActionController::Base
     render :show
   end
 
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(permitted_params)
+
+    if @user.save
+      redirect_to root_path, flash: "New user added succesfully"
+    else
+      render :new
+    end
+  end
+
   def expert_search
     @user = User.find_by_id(params[:id])
     @related_headings = @user.find_related_headings_by_topic(params[:topic])
 
     render partial: 'result_list', locals: { user: @user, headings: @related_headings }, content_type: 'text/html'
+  end
+
+  private
+
+  def permitted_params
+    params.require(:user).permit(:name, :website_url)
   end
 end
